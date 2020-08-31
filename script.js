0c1365f77c9e770d886b2d9b8a18e4291f98d2c4
Hypergolix <66037340+Hypergolix@users.noreply.github.com>
2020-08-31 18:51:00 +0200

    cleaned up

--- script.js
+++ script.js
@@ -12,18 +12,19 @@ async function fetchData(url) {
 function cacheImagesFromAPI() {
 
     numberOf = document.getElementById("imgNumber").value;
+    /*
     if (hasPressed)
         document.getElementById('img').value = "";
-
+    */
     var BASEURL = `https://shibe.online/api/shibes?count=${numberOf}&urls=true&httpsUrls=true/request`;
     const promise = fetchData(BASEURL);
 
     promise.then((response) => {
         images = response.map(image => image);
-        
+
         console.log(images);
 
-        images.forEach(function (image) {             // for each link l in ArrayOfImages
+        images.forEach(function (image) {              // for each link l in ArrayOfImages
             var img = document.createElement('img');   // create an img element
             img.src = image;                           // set its src to the link l
             document.body.appendChild(img);            // append it to the body 
